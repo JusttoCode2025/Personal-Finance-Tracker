7ce4a63f7206df4ffcaed94aa09a400979b9f54c
Keyanna Bernard <keyanna.bernard@my.liu.edu>
2026-03-03 21:27:51 -0500

    Update test_app_routes.py
    Signed-off-by: Keyanna Bernard <keyanna.bernard@my.liu.edu>

--- tests/test_app_routes.py
+++ tests/test_app_routes.py
@@ -1,5 +1,11 @@
-from backend.app import app
+import sys
+import os
 import pytest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'backend')))
+
+from app import app
+
 
 @pytest.fixture
 def client():
